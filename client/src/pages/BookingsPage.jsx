ed45721a748da8ce39710358745274ca4a39f4a6
Yogesh Kaushik <Yogeshkaushik138@gmail.com>
2026-07-12 15:43:20 +0530

    Fix BookingsPage crash: add missing useMutation and useQueryClient imports

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import api from '../api/axios';
 
